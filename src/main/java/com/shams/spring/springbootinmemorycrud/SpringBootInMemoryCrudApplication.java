a8dc3d3ca0d0bdf3b2607fd44e09e90c31fc133c
RightHandedCoder <aniknishan@gmail.com>
2019-07-19 20:23:05 +0600

    init

---
+++ src/main/java/com/shams/spring/springbootinmemorycrud/SpringBootInMemoryCrudApplication.java
@@ -0,0 +1,13 @@
+package com.shams.spring.springbootinmemorycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInMemoryCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInMemoryCrudApplication.class, args);
+    }
+
+}
